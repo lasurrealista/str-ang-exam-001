c07ddd8693efdea7ad32c2581780f4d9736dcc26
lasurrealista <ladyblue@freemail.hu>
2021-02-15 18:29:42 +0100

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { HomeComponent } from './page/home/home.component';
+import { HeroesComponent } from './page/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
